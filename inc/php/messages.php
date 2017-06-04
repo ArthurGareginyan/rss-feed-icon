3ed4dd58c2a51b372a016fde9e65c3d5feb411bb
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-06-04 20:43:19 +0300

    Plugin updated to version 2.4

--- inc/php/messages.php
+++ inc/php/messages.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) or die( "Restricted access!" );
 /**
  * Hello message - Bootstrap Modal
  *
- * @since 2.2
+ * @since 2.4
  */
 function RssFeedIcon_hello_message() {
 
@@ -29,7 +29,7 @@ function RssFeedIcon_hello_message() {
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <p><?php _e( 'Hello. I\'m Arthur, the author of this plugin.', RSSFI_TEXT ); ?></p>
                         <p><?php printf(
-                                        __( 'Thank you for installing my plugin! I hope you will love it %s', RSSFI_TEXT ),
+                                        __( 'Thank you for installing my plugin! I hope you will love it! %s', RSSFI_TEXT ),
                                         '&#x1F603;'
                                         );
                             ?></p>
